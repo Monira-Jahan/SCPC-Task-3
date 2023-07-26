a9e502e4c3c1280b981bec426529bbf5e040c74f
Monira Jahan <monirajahan459@gmail.com>
2023-07-26 21:10:46 +0600

    Problem-6 solved

---
+++ problem-6.js
@@ -0,0 +1,27 @@
+function generatePassword(length) {
+    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers="123456789";
+    var specialChar="!@#$%&*()";
+    var password="";
+    // var passwordLength = 12;
+    // var password = "";
+
+ if(length<8){
+    console.log("Password should be at least 8 characters.")
+ }
+ else{
+    let i=0;
+    while(i<length){
+      password+=chars[Math.floor(Math.random()*chars.length)];
+      password+=numbers[Math.floor(Math.random()*numbers.length)];
+      password+=specialChar[Math.floor(Math.random()*specialChar.length)];
+      i+=3;
+
+    }
+    password=password.substring(0,length);
+    return password;
+ }
+}
+
+const result=generatePassword(8);
+console.log(result);
