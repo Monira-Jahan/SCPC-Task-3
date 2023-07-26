6b91b85509adf3a88e754efe661a15e85c7914bd
Monira Jahan <monirajahan459@gmail.com>
2023-07-26 22:23:14 +0600

    Final Commit

--- problem-3.js
+++ problem-3.js
@@ -1,4 +1,4 @@
-// 3. Write a JavaScript program to find the most frequent element in an array and return it. 
+// Task-3: Write a JavaScript program to find the most frequent element in an array and return it. 
 
 
 var arr=[3, 5, 2, 5, 3, 3, 1, 4, 5];
--- problem-4.js
+++ problem-4.js
@@ -1,4 +1,4 @@
-//4. Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers. 
+//Task-4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers. 
 
 function twoSum(nums, target) {
     for(let i = 0; i < nums.length; i++){
